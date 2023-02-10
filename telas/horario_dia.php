701b1632836eb27bd6e0d020baa07fd264e665db
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-10 08:51:49 -0400

    Atualizações

--- telas/horario_dia.php
+++ telas/horario_dia.php
@@ -2,8 +2,8 @@
 	date_default_timezone_set('America/Manaus');
 	session_start();
 	include("../lib/fn.php");
-
-	$dados = file_get_contents("http://api.aladhan.com/v1/gToH?date=10-02-2023");
+	$hoje = date("d-m-Y");
+	$dados = file_get_contents("http://api.aladhan.com/v1/gToH?date=".$hoje);
 	$dados = json_decode($dados);
 
 	var_dump($dados);
