ebd70630d733ca41e3ac1b268e884ced11c92953
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-10 10:16:48 -0400

    Atualizações

--- telas/horario_dia.php
+++ telas/horario_dia.php
@@ -18,7 +18,7 @@
 		color:#fff;
 		width:45%;
 	}
-	.titulo p{
+	.titulo p, .titulo i{
 		font-size:35px;
 		font-weight:bold;
 	}
@@ -29,12 +29,14 @@
 			<div class="d-flex justify-content-between">
 				<div class="titulo">
 					<button class="btn btn-success w-100">
+						<i class="fa fa-calendar"></i>
 						<p><?=$hijri->weekday->ar?> <?=$hijri->day?> <?=$hijri->month->ar?> <?=$hijri->year?></p>
 						<p><?=$hijri->weekday->en?>, <?=$hijri->day?> <?=$hijri->month->en?> <?=$hijri->year?></p>
 					</button>
 				</div>
 				<div class="titulo">
 					<button class="btn btn-success w-100">
+						<i class="fa fa-calendar"></i>
 						<p><?=dataArabe();?></p>
 						<p><?=dataPort();?></p>
 					</button>
