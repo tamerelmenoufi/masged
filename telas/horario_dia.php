3690efe724c18ced7ab77c7c001627080f806c39
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-11 13:45:29 -0400

    Atualizações

--- telas/horario_dia.php
+++ telas/horario_dia.php
@@ -44,7 +44,7 @@
 
 <div style="position:fixed; top:0; right:0; bottom:0; left:0; padding:10px;">
 
-	<div class="row mt-3">
+	<div class="row">
 		<div class="col-12">
 			<div class="d-flex justify-content-between">
 				<div class="titulo">
@@ -76,7 +76,7 @@
 	</div>
 
 
-	<div class="row mt-3">
+	<div class="row">
 		<div class="col-12 mt-3">
 
 		<?php
